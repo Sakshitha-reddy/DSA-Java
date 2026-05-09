601167166b99defdeab84b53302135c9d75e04a5
Sakshitha-reddy <sakshithareddy111@gmail.com>
2026-05-09 19:44:18 +0530

    initial commit

---
+++ conditional/ifelse.java
@@ -0,0 +1,10 @@
+public class ifelse{
+    public static void main(String[] args){
+        int n=3;
+        if(n>4){
+            System.out.println("greater");
+        }else{
+            System.out.println("lesser");
+        }
+    }
+}
